d627439490b1f84df3dbf587c6f9924a2323f6f5
changchangge123 <changchangge123@qq.com>
2016-11-08 02:04:08 +0800

    2 clock ,finish the click item logic

--- click_item.py
+++ click_item.py
@@ -1,14 +1,23 @@
 #this is test for git bound sublime
-def click_category(self, index):
-	#对于已经展开的list->收回list，设置category的高亮状态。 #并且如果该category是当前选中type的cate，那么取消type的选中状态
+def click_category(self, cate_data, index):
+	#对于已经展开的list->收回list，取消category的高亮状态。 #并且如果该category是当前选中type的cate，那么取消type的选中状态
 	item = self._listview_left.get_item_by_index(index)
-	item.set_select(True)
-	if self.click_category_index == index:
+	if item.is_spreading:
+		if self._clicked_category_index == index and self._clicked_type_index!=None:
+			self.set_select_by_index(self._clicked_type_index, False)
+		self._takeback_list(index)
+
 		if self.click_category_index == index:
 
+
 	#对于没有展开的list,展开list，并将第一个type设置为选中状态
-
-
+	else:
+		category_id = cate_data.get("reputation_category_id")
+		types_of_category = self.get_types_of_category(category_id)
+		for i in range(index+1, index+len(types_of_category)):
+			self._listview_left.insert_one(i, types_of_category[i])
+		if types_of_category:
+			self._listview.get_item_by_index(index+1).on_click(None)
 
 ###明天的任务
 def show_new_species_sigh(self):
