190e0c2416d7418b273d27e565926a249458bb96
changchangge123 <changchangge123@qq.com>
2016-11-08 01:34:51 +0800

    is it the reason of unsupport of chinese?

--- click_item.py
+++ click_item.py
@@ -1,9 +1,11 @@
 #this is test for git bound sublime
 def click_category(self, index):
 	#对于已经展开的list->收回list，设置category的高亮状态。 #并且如果该category是当前选中type的cate，那么取消type的选中状态
-	
+	item = self._listview_left.get_item_by_index(index)
+	item.set_select(True)
 	if self.click_category_index == index:
-		
+		if self.click_category_index == index:
+
 	#对于没有展开的list,展开list，并将第一个type设置为选中状态
 
 
